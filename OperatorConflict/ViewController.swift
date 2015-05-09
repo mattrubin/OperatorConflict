59b4d4cf17357639a6f1c2be9092fc44e9f4caf1
Matt Rubin <opensource@mattrubin.me>
2015-05-09 16:40:34 -0700

    Link, embed, and use Runes

--- OperatorConflict/ViewController.swift
+++ OperatorConflict/ViewController.swift
@@ -8,18 +8,26 @@
 
 import UIKit
 import Result
+import Runes
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         useResult()
+        useRunes()
     }
 
     private func useResult() {
-        let resultString: Result<String, Void> = .success("hello!")
-        let enthusiasticResult = resultString >>- intensifyEnthusiasm
-        print(enthusiasticResult)
+        let possibleString: Result<String, Void> = .success("hello!")
+        let possibleEnthusiasticString = possibleString >>- intensifyEnthusiasm
+        print(possibleEnthusiasticString)
+    }
+
+    private func useRunes() {
+        let possibleString: String? = .Some("hello!")
+        let possibleEnthusiasticString = possibleString >>- intensifyEnthusiasm
+        print(possibleEnthusiasticString)
     }
 
 }
@@ -33,4 +41,14 @@ private func intensifyEnthusiasm(string: String) -> Result<String, Void> {
     }
     // We can't handle such an unenthusiastic string.
     return .failure()
+}
+
+private func intensifyEnthusiasm(string: String) -> String? {
+    // If the string is already enthusiastic...
+    if string[string.endIndex.predecessor()] == "!" {
+        // ...intensify the enthusiasm!
+        return string + "!"
+    }
+    // We can't handle such an unenthusiastic string.
+    return .None
 }
